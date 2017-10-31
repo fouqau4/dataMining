0297a539f6c9607244483596b7f0ddd2b0e250af
fouqau4 <fouqau4@gmail.com>
2017-10-31 16:25:53 +0800

    update

--- hw/hw1/Asso/main.cpp
+++ hw/hw1/Asso/main.cpp
@@ -1,20 +1,29 @@
 #include "FPGrowth.h"
 #include <iostream>
 #include <string>
+
+#include <ctime>
 	
 using std::cout;
 using std::endl;
 using std::string; using std::stod;
 
+using std::time; using std::time_t; using std::difftime;
+
 int main( int argc, char* argv[] )
 {
+	time_t beg;
 	switch( argc )
 	{
 	case 2:
+		beg = time( NULL );
 		FPGrowth( argv[1], 0.1f );
+		cout << difftime( time( NULL ), beg ) << endl;
 		break;
 	case 3:
+		beg = time( NULL );
 		FPGrowth( argv[1], stod( string( argv[2] ) ) );
+		cout << difftime( time( NULL ), beg ) << endl;
 		break;
 	default:
 		// default case
